d3e85606a951b374545d01e13ff68d4c42855b77
Abdur Rachman Wahed <abdurrachmanwahed@gmail.com>
2024-05-19 21:08:42 +0700

    feat: add navigation

---
+++ standups/standups/Modules/App/AppStore.swift
@@ -0,0 +1,8 @@
+//
+//  AppStore.swift
+//  standups
+//
+//  Created by Abdur Rachman Wahed on 12/05/24.
+//
+
+import Foundation
---
+++ standups/standups/Modules/App/AppView.swift
@@ -0,0 +1,18 @@
+//
+//  AppView.swift
+//  standups
+//
+//  Created by Abdur Rachman Wahed on 12/05/24.
+//
+
+import SwiftUI
+
+struct AppView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AppView()
+}
